c1cd880c3f9ce037e8463e315f703aa42d4537a2
shunpin tseng <shunpin@gmail.com>
2020-11-13 13:01:52 -0800

    map fix

--- src/pages/FoodBank/Map/API.js
+++ src/pages/FoodBank/Map/API.js
@@ -1,6 +1,6 @@
 const express = require("express");
 const axios = require("axios");
-const db = require("")// what's the path to database to get foodbank address
+const db = require("./node_modules")// what's the path to database to get foodbank address
 
 
 
