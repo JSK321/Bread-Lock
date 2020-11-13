7e42245667de98c4c59a7eae59075d1a3de28fe5
shunpin tseng <shunpin@gmail.com>
2020-11-13 11:10:30 -0800

    npm install leaflet

--- src/pages/FoodBank/Map/index.js
+++ src/pages/FoodBank/Map/index.js
@@ -6,51 +6,19 @@ import React from 'react';
 
 export default function Map() {
     return (
-            <div id="mapid" style="height: 600px; position: relative">
-
-                <script>
-                    var mymap = L.map('map').setView([47.6062,122.3321], 18);
-
-                    L.tileLayer('https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_token=pk.eyJ1IjoibWFwYm94IiwiYSI6ImNpejY4NXVycTA2emYycXBndHRqcmZ3N3gifQ.rJcFIG214AriISLbB6B5aw', {
-                        maxZoom: 18,
-                        attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, ' +
-                            '<a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, ' +
-                            'Imagery © <a href="https://www.mapbox.com/">Mapbox</a>',
-                        id: 'mapbox/streets-v11',
-                        tileSize: 512,
-                        zoomOffset: -1
-                    }).addTo(mymap);
-
-
-
-                    //custom icon setting for food bank locations
-                    {/* var fBankIcon = L.icon({
-                        iconUrl: '',
-                        iconSize: [50, 50],
-                        iconAnchor: [22, 94],
-                        popupAnchor: [-3, -76],
-                        shadowUrl: '',
-                        shadowSize: [0, 0],
-                        shadowAnchor: [22, 94]
-                    });
-                    //add markers for multiple garden locations:
-                    {{#each fBankPins }}
-                    L.marker([{{ location }}], { icon: fBankIcon }).addTo(mymap).bindPopup(" ").openPopup();
-                    {{/each}} */}
-
-                
-
-              
-                    {/* var popup = L.popup(); */}
-
-
-                </script>
-
-
-            </div>
-          
-
-
+div>
+<MapContainer center={[51.505, -0.09]} zoom={13} scrollWheelZoom={false}>
+  <TileLayer
+    attribution='&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
+    url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+  />
+  <Marker position={[51.505, -0.09]}>
+    <Popup>
+      A pretty CSS3 popup. <br /> Easily customizable.
+    </Popup>
+  </Marker>
+</MapContainer>
+</div>
     )
 }
 
