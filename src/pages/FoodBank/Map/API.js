af628a58f1992f0ed88d263604d9b31a5188ac88
shunpin tseng <shunpin@gmail.com>
2020-11-14 20:06:03 -0800

    map icon update

--- src/pages/FoodBank/Map/API.js
+++ src/pages/FoodBank/Map/API.js
@@ -1,24 +1,36 @@
-const express = require("express");
+//const express = require("express");
 const axios = require("axios");
-const db = require("./node_modules")// what's the path to database to get foodbank address
+//const db = require("./node_modules")// what's the path to database to get foodbank address
 
 
+
+
+const API = {
+  getFoodbanks:function(){
+    return axios
+    .get(
+      `http://localhost:8080/api/foodbank/get/all`
+    );
+  }
+}
+
+module.exports = API;
 
 //set var for location lat and long
-var lat;
-var long;
+// var lat;
+// var long;
 
-//map search function to convert address to lat & long code
-function geoData(req, res) {
-  let location = req.body.address
-  const APIKey = "51966f60-25d5-11eb-a940-51e78db4786d"; 
-  axios
-    .get(
-      `https://app.geocodeapi.io/api/v1/search?apikey=${APIKey}&text=${location}`
-    )
-    .then((res) => {
-      console.log(res);
-      var lat = response.data.bbbox[1];
-      var long = response.data.bbox[0];
-    });
-};
+// //map search function to convert address to lat & long code
+// function geoData(req, res) {
+//   let location = req.body.address
+//   const APIKey = "51966f60-25d5-11eb-a940-51e78db4786d"; 
+//   axios
+//     .get(
+//       `https://app.geocodeapi.io/api/v1/search?apikey=${APIKey}&text=${location}`
+//     )
+//     .then((res) => {
+//       console.log(res);
+//       var lat = response.data.bbbox[1];
+//       var long = response.data.bbox[0];
+//     });
+// };
--- src/pages/FoodBank/Map/index.js
+++ src/pages/FoodBank/Map/index.js
@@ -1,22 +1,52 @@
-import React from 'react';
+import React, {useState} from "react"
 import { Marker, Popup, TileLayer, MapContainer } from "react-leaflet";
+import API from "./API"
+
+import icon from 'leaflet/dist/images/marker-icon.png';
+import iconShadow from 'leaflet/dist/images/marker-shadow.png';
+import L from 'leaflet';
 import 'leaflet/dist/leaflet.css';
 
 
+
+let DefaultIcon = L.icon({
+  iconUrl: icon,
+  shadowUrl: iconShadow
+});
+L.Marker.prototype.options.icon = DefaultIcon;
+
+//import './style.css';
+
+ 
+
 export default function Map() {
+
+    const [bankState, setBankState] = useState({
+       data:"",
+       dataLoaded:false
+     })
+
+  API.getFoodbanks().then((res) => {
+    setBankState({
+      data:res.data,
+      dataLoaded:true
+    })
+  });
+
+
+
+  // let mapDataWLocation = 
+
+
     return (
       
       
-      <MapContainer center={[47.6, 122.2]} zoom={13} scrollWheelZoom={false} style={{ height: '50vh', width: '50vh' }}>
+      <MapContainer center={[47.6,-122.3]} zoom={18} scrollWheelZoom={false} style={{ height: '50vh', width: '50vh' }}>
       <TileLayer
         attribution='&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
         url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
       />
-      <Marker position={[51.505, -0.09]}>
-        <Popup>
-          A pretty CSS3 popup. <br /> Easily customizable.
-        </Popup>
-      </Marker>
+      {bankState.dataLoaded?bankState.data.map(entry=><Marker position={[entry.latutude,entry.longitude]}> <Popup>{entry.bankName}</Popup> </Marker>) : <p>hola</p>}
     </MapContainer>
  
     )
