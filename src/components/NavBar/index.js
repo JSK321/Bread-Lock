ea687ed78791526417972bbb4772c6d5b04ec20b
Jae Kim <jaeshinkim321@gmail.com>
2020-11-16 11:04:10 -0800

    working navbar

--- src/components/NavBar/index.js
+++ src/components/NavBar/index.js
@@ -3,24 +3,41 @@ import React from 'react'
 
 export default function NavBar() {
     return (
-        <nav className="uk-navbar-container" uk-navbar>
+        <nav className="uk-navbar-container" uk-navbar="dropbar:true">
             <div className="uk-navbar-left">
-
                 <ul className="uk-navbar-nav">
-                    <li><a href="#">Active</a></li>
+                    <li><a href="/">Home</a></li>
+                    <li><a href="/map">Map</a></li>
                     <li>
-                        <a href="#">Parent</a>
-                        <div className="uk-dropdown">
+                        <a href="#">View Pantry</a>
+                        <div className="uk-navbar-dropdown">
                             <ul className="uk-nav uk-navbar-dropdown-nav">
-                                <li className="uk-active"><a href="#">Active</a></li>
-                                <li><a href="#">Item</a></li>
-                                <li><a href="#">Item</a></li>
+                                <li><a href="/customerorder">Order Page</a></li>
+                                <li><a href="/pantrydata">Available Food</a></li>
+                                <li><a href="/customerqueue">Recipient Queue List</a></li>
                             </ul>
                         </div>
                     </li>
-                    <li><a href="#">Item</a></li>
+                    <li>
+                        <a href="#">Profile</a>
+                        <div className="uk-navbar-dropdown">
+                            <ul className="uk-nav uk-navbar-dropdown-nav">
+                                <li><a href="/profile">Profile Page</a></li>
+                                <li><a href="/pantry">Food Bank Pantry</a></li>
+                            </ul>
+                        </div>
+                    </li>
+                    <li>
+                        <a href="#">Sign Up</a>
+                        <div className="uk-navbar-dropdown">
+                            <ul className="uk-nav uk-navbar-dropdown-nav">
+                                <li><a href="/adminhome">Admin Home Page</a></li>
+                                <li><a href="/fbsignup">Register Food Bank</a></li>
+                                <li><a href="/signup">Register as Recipient</a></li>
+                            </ul>
+                        </div>
+                    </li>
                 </ul>
-
             </div>
         </nav>
     )
