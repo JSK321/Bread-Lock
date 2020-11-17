1f99b81a55b4f64c3febd3fe62accd4da2fc7cf2
shunpin tseng <shunpin@gmail.com>
2020-11-17 13:38:25 -0800

    homepage Setup

--- src/pages/Admin/PantryData/index.js
+++ src/pages/Admin/PantryData/index.js
@@ -2,7 +2,7 @@ import React, { useEffect, useState } from "react";
 import { useParams } from "react-router-dom";
 import API from "../../../utils/API";
 
-export default function FoodBank() {
+export default function PantryData() {
   const [foodBank, setFoodBank] = useState({});
 
   const { id } = useParams();
