ce3fa5a3e658d571ec51b6401b5dd75929cb76fa
shunpin tseng <shunpin@gmail.com>
2020-11-18 11:27:49 -0800

    orderPageLink

--- src/components/Footer/index.js
+++ src/components/Footer/index.js
@@ -3,7 +3,7 @@ import logo from "../../images/BreadLockLogo.png";
 
 export default function Footer() {
   return (
-    <nav className="uk-uk-navbar-container" style={{ textAlign: "center" }}>
+    <nav className="uk-uk-navbar-container" uk-navbar="true" style={{ margin:"5%"}}>
       <div className="uk-navbar-center">
         <img className="BreadLock Logo" src={logo} alt="logo" width="150px" />
       </div>
