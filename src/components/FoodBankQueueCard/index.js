42d8df0cbefa98dcdd0caa58fc4df8937e1e0c05
Jae Kim <jaeshinkim321@gmail.com>
2020-11-18 13:37:50 -0800

    adding seeding infor

--- src/components/FoodBankQueueCard/index.js
+++ src/components/FoodBankQueueCard/index.js
@@ -28,17 +28,31 @@ export default function FoodBankQueueCard() {
             <div className="uk-card uk-card-default uk-margin-left uk-width-expand">
                 <h4 style={{ textAlign: "center" }}>Customer Queue</h4>
                 <ul>
-                    {foodBankQueueCard.customerInfo.map((data =>
-                        <li>
-                            Date: {data.orderDate}
-                            <br></br>
-                            Pick Up Time: {data.orderTime}
-                        </li>
-                    ))}
+                    {
+                        foodBankQueueCard.customerInfo != undefined ? (
 
-                    <li>
-                        Map over customer queue list. <button>View Customer Order</button>
-                    </li>
+                            foodBankQueueCard.customerInfo.map((data =>
+                                <li>
+                                    Name: {data.Customer.firstName} {data.Customer.lastName}
+                                    <br></br>
+                                    Date: {data.orderDate}
+                                    <br></br>
+                                    Pick Up Time: {data.orderTime}
+                                    <br></br>
+
+                                    {/* <button className="uk-button uk-button-default uk-margin-small-right" type="button" uk-toggle="target: #modal-close-default">Default</button>
+
+                                    <div id="modal-close-default" uk-modal>
+                                        <div className="uk-modal-dialog uk-modal-body">
+                                            <button className="uk-modal-close-default" type="button" uk-close></button>
+                                            <h2 className="uk-modal-title">Default</h2>
+                                            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+                                        </div>
+                                    </div> */}
+                                </li>
+                            ))
+                        ) : null
+                    }
                 </ul>
             </div>
         </div>
