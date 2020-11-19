eedddb1272afb82be260b3fe64fcc45033a56afa
shunpin tseng <shunpin@gmail.com>
2020-11-19 10:55:25 -0800

    RecoveryFile

--- src/components/Footer/index.js
+++ src/components/Footer/index.js
@@ -1,11 +1,16 @@
 import React from "react";
 import logo from "../../images/BreadLockLogo.png";
-
+import background from "../../images/background.jpg"
 export default function Footer() {
+    let sectionStyle = {
+    backgroundImage: "url(" +  background  + ")",
+    width: "100%"
+    };
   return (
-    <nav className="uk-uk-navbar-container" uk-navbar="true" style={{ margin:"5%"}}>
-      <div className="uk-navbar-center">
-        <img className="BreadLock Logo" src={logo} alt="logo" width="150px" />
+    <nav className="uk-navbar-container uk-background-cover" uk-navbar="true" style={sectionStyle}>
+      <div className="uk-navbar-center"  >
+        <img className="uk-padding-small" src={logo} alt="logo" width="200px"/>
+    
       </div>
     </nav>
   );
--- src/pages/User/Home/index.js
+++ src/pages/User/Home/index.js
@@ -1,49 +1,53 @@
 import React from "react";
 import { Link } from "react-router-dom";
-import logo from "../../../images/BreadLockLogo.png";
+import homepageImg from "../../../images/homepageImage.jpg";
 
 export default function Home() {
   return (
     <div className="uk-flex uk-flex-center">
-      <div className="uk-card uk-card-default uk-width-1-2@m" style ={{padding:"5%"}}>
-      
-        <img className="BreadLock Logo" src={logo} alt="logo" width="80%" />
-     
-        <h3>Welcome to BreadLock</h3>
-        <h5 style={{ textAlign: "left" }}>
-          Bread Lock is an application that connects food banks with the
-          receivers in the Seattle area. Food banks in the Seattle area are
-          mostly independent from each other. Each bank has their own website
-          but is limited in information of their amount of food in their pantry.
-          Bread Lock aims to gather information on the availability of food each
-          bank has in their pantry and be able to distribute them to people who
-          are in need. Receivers will be able to sign up to secure their share
-          of food from the food bank and schedule a timeframe to pick it up.
-          This will help reduce waste as the food bank will be able to measure
-          how much food to distribute depending on the amount of receivers
-          signed up. It will also help reduce contact between people during this
-          ongoing pandemic as people will not need to wait in lines to receive
-          their share. With the latest technology of React, Bread Lock functions
-          on a real time basis, allowing receivers to feel assured as they know
-          their share of food will be secured for them!
-          <br />
-          Main goal is to aid in our community through uniformity and
-          connectivity while maintaining social distancing.
-        </h5>
-
+      <div className="uk-card uk-card-default uk-width-1-2@m">
+        <img
+          className="hompageImg"
+          src={homepageImg}
+          alt="homepage"
+          width="100%"
+        />
+        <div style={{ padding: "5%" }}>
+          <h3>Welcome to BreadLock</h3>
+          <h5 style={{ textAlign: "left" }}>
+            Bread Lock is an application that connects food banks with the
+            receivers in the Seattle area. Food banks in the Seattle area are
+            mostly independent from each other. Each bank has their own website
+            but is limited in information of their amount of food in their
+            pantry. Bread Lock aims to gather information on the availability of
+            food each bank has in their pantry and be able to distribute them to
+            people who are in need. Receivers will be able to sign up to secure
+            their share of food from the food bank and schedule a timeframe to
+            pick it up. This will help reduce waste as the food bank will be
+            able to measure how much food to distribute depending on the amount
+            of receivers signed up. It will also help reduce contact between
+            people during this ongoing pandemic as people will not need to wait
+            in lines to receive their share. With the latest technology of
+            React, Bread Lock functions on a real time basis, allowing receivers
+            to feel assured as they know their share of food will be secured for
+            them!
+            <br />
+            Main goal is to aid in our community through uniformity and
+            connectivity while maintaining social distancing.
+          </h5>
+        </div>
         <div style={{ textAlign: "center" }}>
           <Link to={"/map"}>
             <button>Foodbanks near me</button>
           </Link>
         </div>
-        <br/>
+        <br />
         <div style={{ textAlign: "center" }}>
           <Link to={"/signup"}>
             <button>Sign Up</button>
           </Link>
         </div>
-
-
+        <br />
       </div>
     </div>
   );
