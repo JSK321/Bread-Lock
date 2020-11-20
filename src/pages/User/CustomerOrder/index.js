5076ee7ac0ac97da28963b1288e60eb12dd9e026
shunpin tseng <shunpin@gmail.com>
2020-11-20 00:09:21 -0800

    urlpointer updates

--- src/pages/User/CustomerOrder/index.js
+++ src/pages/User/CustomerOrder/index.js
@@ -4,7 +4,7 @@ import CustomerOrderForm from '../../../components/CustomerOrderForm'
 import CustomerPickUpForm from '../../../components/CustomerPickUpForm';
 // import foods from '../../../foods.json';
 // import FoodBank from '../../FoodBank/FoodBankDetails';
-import API, { postOneOrderItem, putOnePantryItem } from "../../../utils/API"
+import API from "../../../utils/API"
 
 const URL_PREFIX = "http://localhost:8080"
 // const URL_PREFIX = "https://breadlockapi.herokuapp.com"
@@ -89,7 +89,7 @@ export default function CustomerOrder() {
             if (parseInt(customerOrder.foodList[j].id) === parseInt(id)) {
                 let newClaimed = parseInt(customerOrder.foodList[j].claimed) + 1;
                 let newNotClaimed = parseInt(customerOrder.foodList[j].notClaimed) - 1;
-                putOnePantryItem(newClaimed, newNotClaimed, id);
+                API.putOnePantryItem(newClaimed, newNotClaimed, id);
             }
         }
     }
@@ -171,7 +171,7 @@ export default function CustomerOrder() {
                             const lastMade = user.id;
                             // Fix the order ids
                             stockArray.forEach(element => {
-                                postOneOrderItem(1, lastMade, element);
+                                API.postOneOrderItem(1, lastMade, element);
                             });
                             afterOrder();
                             window.location.href = `${URL_REDIRECT}/`;
---
+++ src/utils/urlPointer.js
@@ -0,0 +1,4 @@
+export const URL_PREFIX = "http://localhost:8080"
+// export const URL_PREFIX = "https://breadlockapi.herokuapp.com"
+export const URL_REDIRECT = "http://localhost:3000"
+// export const URL_REDIRECT = "https://breadlock.herokuapp.com"
