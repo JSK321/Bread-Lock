11eae08a716b88b5a8f257bfcf2ecdfc8586da2c
Jae Kim <jaeshinkim321@gmail.com>
2020-11-21 13:35:14 -0800

    combining food bank information

--- src/pages/FoodBank/FoodBankDetails/index.js
+++ src/pages/FoodBank/FoodBankDetails/index.js
@@ -1,18 +1,40 @@
 import React, { useEffect, useState } from "react";
 import { Link, useParams } from "react-router-dom";
 import API from "../../../utils/API";
-
+import { URL_PREFIX, URL_REDIRECT } from "../../../utils/urlPointer"
 
 export default function FoodBank(props) {
-  const [foodBank, setFoodBank] = useState({});
 
   const { id } = useParams();
 
-  useEffect(() => {
+  const [foodBank, setFoodBank] = useState({});
+
+  const [foodBankPantry, setFoodBankPantry] = useState({
+    foodList: [],
+    FoodBankPantryId: id,
+  });
+
+  function loadPantry() {
+    API.getOneFBPantry(id).then((res) => {
+      setFoodBankPantry({
+        foodList: res,
+        FoodBankPantryId: id,
+      });
+    });
+  }
+
+  function loadFoodBank() {
     API.getOneFoodBank(id).then((res) => {
       setFoodBank(res);
     });
+  }
+
+  useEffect(() => {
+    loadPantry()
+    loadFoodBank()
+
   }, []);
+
   return (
     <div className="uk-flex uk-flex-center">
       <div className="uk-card uk-card-default uk-width-1-2@m">
@@ -23,37 +45,51 @@ export default function FoodBank(props) {
             </div>
           </div>
         </div>
-        <div className="uk-card-body" style={{ textAlign: "justify", margin: "0 auto", width: "30em" }}>
+        <div className="uk-flex uk-flex-center uk-flex-around">
+          <div className="uk-card uk-card-default uk-card-body" style={{ textAlign: "justify", margin: "0 auto", width: "30em" }}>
+            <ul>
+              <li>Address: {foodBank.address}</li>
+              <br></br>
+              <li>City: {foodBank.cityName}</li>
+              <br></br>
+              <li>State: {foodBank.stateAbr}</li>
+              <br></br>
+              <li>ZipCode: {foodBank.zipCode}</li>
+              <br></br>
+              <li>Phone: {foodBank.phone}</li>
+              <br></br>
+              <li>Email: {foodBank.email}</li>
+              <br></br>
+            </ul>
+          </div>
+        </div>
+
+        <div className="uk-card uk-card-default uk-card-body" style={{ textAlign: "justify", margin: "0 auto", width: "30em" }}>
           <ul>
-            <li>Address: {foodBank.address}</li>
-            <br></br>
-            <li>City: {foodBank.cityName}</li>
-            <br></br>
-            <li>State: {foodBank.stateAbr}</li>
-            <br></br>
-            <li>ZipCode: {foodBank.zipCode}</li>
-            <br></br>
-            <li>Phone: {foodBank.phone}</li>
-            <br></br>
-            <li>Email: {foodBank.email}</li>
-            <br></br>
+            {foodBankPantry.foodList.map((pantryList) => (
+              <li style={{ textAlign: "center" }}>
+                {pantryList.Stock.stockName} Portion Available:{" "}
+                {pantryList.notClaimed}
+              </li>
+            ))}
           </ul>
         </div>
+
         <div className="uk-card-footer" style={{ textAlign: "center" }}>
           <Link to={'/pantry/' + id}>
             <button>View Pantry</button>
           </Link>
           <br></br>
           <br></br>
-          {props.isLoggedIn ? 
-          <Link to={"/customerorder/" + foodBank.FoodBankId}>
+          {props.isLoggedIn ?
+            <Link to={"/customerorder/" + foodBank.FoodBankId}>
               <button>Order Form</button>
-            </Link> : 
+            </Link> :
             <Link to={"/signin"}>
               <button>Sign In</button>
             </Link>}
-            <br></br>
-            {props.isLoggedIn ? null : <Link to="/signup"><button>Sign Up</button></Link>}
+          <br></br>
+          {props.isLoggedIn ? null : <Link to="/signup"><button>Sign Up</button></Link>}
           <br></br>
           <Link to={'/foodbankqueue/' + id}>
             <button>View Customer Queue</button>
