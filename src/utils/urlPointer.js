9506f59dbd83fae0b8dba63ffd27b224515e4bf8
Jae Kim <jaeshinkim321@gmail.com>
2020-11-22 14:46:39 -0800

    changed routes

--- src/utils/urlPointer.js
+++ src/utils/urlPointer.js
@@ -1,4 +1,4 @@
-export const URL_PREFIX = "http://localhost:8080"
-// export const URL_PREFIX = "https://breadlockapi.herokuapp.com"
-export const URL_REDIRECT = "http://localhost:3000"
-// export const URL_REDIRECT = "https://breadlock.herokuapp.com"
+// export const URL_PREFIX = "http://localhost:8080"
+export const URL_PREFIX = "https://breadlockapi.herokuapp.com"
+// export const URL_REDIRECT = "http://localhost:3000"
+export const URL_REDIRECT = "https://breadlock.herokuapp.com"
